f9c46b5d335db429c0cd9cff6e44304a66a6e0e7
Supakit Thanadittagorn <skyfox.ku@gmail.com>
2016-08-23 18:30:15 +0700

    Test performSegueWithIdentifier

--- TestSegue/ViewController.swift
+++ TestSegue/ViewController.swift
@@ -10,16 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    lazy var closure: (String, AnyObject?) -> Void = self.performSegueWithIdentifier
+
+    @IBAction func go() {
+        closure("GoPage2", nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
--- TestSegueTests/TestSegueTests.swift
+++ TestSegueTests/TestSegueTests.swift
@@ -21,16 +21,15 @@ class TestSegueTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    func testSegue() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewControllerWithIdentifier("ViewController") as! ViewController
+
+        controller.closure = {
+            (identifier: String,_) in
+            XCTAssertTrue(identifier == "GoPage2")
         }
+        controller.go()
+
     }
-    
 }
